bb89efea0c3ed92a086a4203a65754d9d19bcc2d
Kevin Andrade <86170688+Kvble@users.noreply.github.com>
2022-08-04 15:27:36 +0200

    Added first style of navbar menu

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import './NavBar.css';
+import NavMenu from './NavMenu';
+
+function NavBar() {
+  return (
+    <div className="navbar">
+        <p id="title">Kevin Xavier Andrade</p>
+        <NavMenu />
+    </div>
+  );
+}
+
+export default NavBar;
